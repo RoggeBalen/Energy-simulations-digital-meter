456d4eb3db6c4b0da39dd295cb0bc39759f9e0f0
Jonas Meijerink <jonas.meijerink@vito.be>
2026-01-03 23:01:23 +0100

    Update on index and simualtion costprice

--- src/batterysim/Home_battery_simulation.R
+++ src/batterysim/Home_battery_simulation.R
@@ -6,10 +6,10 @@
 
 
 simulate_battery <- function(data, batt_cap = 0, tarief = "vast", energie_kost = energy_cost_ecopower) {
-  # data: dataframe 
-  # batt_cap: 0,5,10 kWh
-  # tarief: "vast" of "variabel" of "dynamisch"
-  # energie_kost: 
+  # Data: een dataframe bestaande uit injectie, verbuik en tijd gegevens
+  # Batt_cap: capaciteit van de batterij
+  # Tarief: "vast" of "variabel" of "dynamisch"
+  # Energie_kost: functie om de kostprijs van het desbetreffende tarief te berekenen
   
   
   n <- nrow(data)
@@ -19,77 +19,80 @@ simulate_battery <- function(data, batt_cap = 0, tarief = "vast", energie_kost =
   
   for (i in 1:(n - 1)) {
     
-    ### Define the difference
-    #------------------------
-    diff <- data$Verbruik[i] - data$Injectie[i]
+    
+    ### Bekijk huidige batterij
+    #--------------------------
+    batt_now <- Batt[i]
     
     
-    ### Meer verbuik dan productie
-    #-----------------------------
-    if (diff > 0) {
+    ### Kostprijs
+    #------------
+    delta_cost <- 0
+    
+    
+    ### Batterij einde interval
+    #--------------------------
+    batt_next <- batt_now
+    
+    
+    ### Energie verbruikt
+    #--------------------
+    if (data$Verbruik[i] > 0) {
       
       
       ### Energie gebruikt van de batterij
       #-----------------------------------
-      used_from_batt <- min(diff, Batt[i])
+      used_from_batt <- min(data$Verbruik[i], Batt[i])
       
       
       ### Update batterij capaciteit
       #-----------------------------
-      Batt[i + 1] <- Batt[i] - used_from_batt
+      batt_next <- batt_next - used_from_batt
       
       
       ### Overige energie die van het net komt
       #---------------------------------------
-      remaining <- diff - used_from_batt    # 0 of >0
+      remaining <- data$Verbruik[i] - used_from_batt    # 0 of >0
       
       
       ### Definieer de kost voor net energie
       #-------------------------------------
       if (remaining > 0) {
-        kost[i + 1] <- kost[i] + energie_kost(remaining, data$start_time[i], tarief)
-      }
-      else{
-        kost[i+1] <- kost[i]
+        delta_cost <- delta_cost + energie_kost(remaining, data$start_time[i], tarief)
       }
     }
     
     
-    ### Meer geproduceerd dan vebruikt
-    #---------------------------------
-    if (diff <= 0) {
-      
-      
-      ### Verschil dat kan worden opgeslagen
-      #-------------------------------------
-      surplus <- abs(diff)
+    ### Meer geproduceerd dan vebruikt dus injectie
+    #----------------------------------------------
+    if (data$Injectie[i] > 0) {
       
       
       ### Capaciteit beschikbaar om op te slagen
       #-----------------------------------------
-      space <- batt_cap - Batt[i]  # remaining battery capacity
-      stored <- min(surplus, space)  # store only what fits
+      space <- batt_cap - Batt[i]             # Overige capaciteit om op te vullen
+      stored <- min(data$Injectie[i], space)  # Slaag zoveel mogelijk op
       
       
       ### Update nieuwe batterij capaciteit
       #------------------------------------
-      Batt[i + 1] <- Batt[i] + stored
+      batt_next <- batt_next + stored
       
       
       ### Overge energie
       #-----------------
-      remaining <- surplus - stored
+      remaining <- data$Injectie[i] - stored
       
       
-      ### Optional sell surplus back to net
-      #------------------------------------
+      ### Optie verkoop overige energie aan het net (negatieve kost)
+      #-------------------------------------------------------------
       if (remaining > 0) {
-        kost[i+1] <- kost[i] + energie_kost(-remaining, data$start_time[i], tarief) # Klopt deze functie???
-      }
-      else{
-        kost[i+1] <- kost[i]
+        delta_cost <- delta_cost + energie_kost(-remaining, data$start_time[i], tarief)
       }
     }
+    
+    Batt[i + 1] <- batt_next
+    kost[i + 1] <- kost[i] + delta_cost
   }
   
   
